56f0f0b2b35ce1e1fc6df113c2a2738ef9724e62
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-06-19 18:50:29 +0200

    Updated update script

--- update.sh
+++ update.sh
@@ -5,9 +5,37 @@
 #
 # This script updates the latest version of bambus.
 
-# Fetch latest changes
+update_dir=.muffin-update
+temp_dir=.muffin-temp
+dirs=(css font ico img _includes js _layouts)
 
-git pull bambus/master
+echo "Preparing update.."
+
+mkdir $temp_dir -p
+
+for dir in $dirs
+	mkdir $temp_dir/$dir -p
+	cp -a $dir/* $temp_dir/$dir  
+done
+
+echo "Download recent version.."
+
+git clone https://github.com/MyRealityCoding/muffin-bambus.git $update_dir
+
+echo "Copying files.."
+
+for dir in $dirs
+        rm $dir/*
+	cp -a $update_dir/$dir $dir
+done
+
+echo "Cleanup.."
+
+rm -r $update_dir
+rm -r $temp_dir
+
+echo "Done."
+
 
 
 
