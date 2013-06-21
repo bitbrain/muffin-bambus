b5162d650e950ed68b423c26fddd7df06985896f
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-06-21 13:21:05 +0200

    Extended install script

--- install.sh
+++ install.sh
@@ -4,22 +4,74 @@
 # Author: Miguel Gonzalez <miguel-gonzalez@gmx.de>
 # Version: 1.0
 
-echo "Install bambus theme.."
+function prepare {
 
-# Dependency: Jekyll
+	# Check dependencies: Git
 
-gem install jekyll
+	if [[ ! -x "`which git`" ]]; then
+		echo "Sorry, but you need to have git installed for installation to proceed."
+		echo ""
+		echo "Ubuntu: sudo apt-get install git-core"
+		echo "OS X: brew install git"
+		echo ""
+		exit 1
+	fi
 
-# Dependency Sass
+	# Check dependencies: Ruby
 
-gem install sass
+	if [[ ! -x "`which ruby`" ]]; then
+		echo "Sorry, but you need to have ruby installed for installation to proceed."
+		echo ""
+		echo "Ubuntu: sudo apt-get install ruby"
+		echo "OS X: brew install ruby"
+		echo ""
+		exit 1
+	fi
 
-echo "Building your site.."
+	# Check dependencies: Ruby Gems
 
-sh build.sh
+	if [[ ! -x "`which gem`" ]]; then
+		echo "Sorry, but you need to have ruby installed for installation to proceed."
+		echo ""
+		echo "Ubuntu: sudo apt-get install rubygems"
+		echo "OS X: brew install rubygems"
+		echo ""
+		exit 1
+	fi
 
-echo "Running your site on http://localhost:4000/"
+	echo ""
+	read -p "Welcome to bambus theme installation! Do you want to install bambus now? [Y/N]?" f
+	[[ "$f" == y* ]]
+}
 
-jekyll serve &
+function install {
+	echo "Install bambus theme.."
 
-echo "Done."
+	# Dependency: Jekyll
+
+	gem install jekyll
+
+	# Dependency Sass
+
+	gem install sass
+
+	echo "Building your site.."
+
+	sh build.sh
+}
+
+function run {
+	echo "Running your site on localhost (port: 4000).."
+	jekyll serve &
+	echo "Your site has been successfully deployed on http://localhost:4000/"
+}
+
+if intro; then
+	install
+	run	
+	echo "Done."
+else
+	echo "Aborted."
+fi
+
+
